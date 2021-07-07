838755181f23a762832ccc5a49f121fabe9917ca
yodsanorin <86304402+yodsanorin@users.noreply.github.com>
2021-07-07 15:03:29 +0700

    [U3] views

--- u3-views/app.js
+++ u3-views/app.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const expHbs = require('express-handlebars')
+const { courseDetails, courseUnits, courseStudents } = require('./features/courseController')
+const { index } = require('./features/indexController')
 
 const app = express()
+
+// Templates
+app.set('views', './views')
+app.set('view engine', 'hbs')
+app.engine('hbs', expHbs({
+  extname: '.hbs',
+  defaultLayout: false,
+  partialsDir: ['./views/partials', './views/layouts']
+}))
 
 // Middleware
 app.use(express.static('public'))
 
 // Routes
-// TODO later
+app.get('/', index)
+app.get('/courses/:code', courseDetails)
+app.get('/courses/:code/units', courseUnits)
+app.get('/courses/:code/students', courseStudents)
 
 const PORT = 3000
-app.listen(PORT,
-  () => console.log(`Listening: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Listening: http://localhost:${PORT}`))
