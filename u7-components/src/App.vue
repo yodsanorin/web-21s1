f3981892a8d60b15d6114c06e979fd550021cf4c
yodsanorin <86304402+yodsanorin@users.noreply.github.com>
2021-08-25 15:42:09 +0700

    [u7]components

---
+++ u7-components/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <span class="brand">
+        <router-link :to="{ name: 'Home' }">DBTube</router-link>
+      </span>
+      <span class="auth">Login</span>
+    </div>
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
