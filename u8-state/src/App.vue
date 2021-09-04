53fd64786b74169e41036e3a9e95bbc733ca1405
yodsanorin <86304402+yodsanorin@users.noreply.github.com>
2021-09-04 22:23:49 +0700

    [U8] State

--- u8-state/src/App.vue
+++ u8-state/src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <span class="brand">
+        <router-link :to="{ name: 'VideoHome' }">DBTube</router-link> /
+        <router-link :to="{ name: 'BookSearch' }">Books</router-link>
+      </span>
+      <div>
+        <router-link
+          :to="{ name: 'VideoPlaylist', params: { slug: 'watch-later' } }"
+        >
+          <img src="/images/icons/playlist-play.png" alt="Watch Later" />{{
+            $store.getters.watchLaterCount
+          }}
+        </router-link>
+        <router-link :to="{ name: 'BookCart' }">
+          <img src="/images/icons/cart-variant.png" alt="Cart" />{{
+            $store.getters.cartCount
+          }}
+        </router-link>
+      </div>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
