57cf282ea9e4be76e4cc33537bfc23fa22411ef5
yodsanorin <86304402+yodsanorin@users.noreply.github.com>
2021-09-11 02:29:39 +0700

    [u9] add Buefy and mdi

--- u9-css/src/main.ts
+++ u9-css/src/main.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@mdi/font/css/materialdesignicons.min.css'
+import './main.scss'
 
 Vue.config.productionTip = false
+Vue.use(Buefy)
 
 new Vue({
   router,
