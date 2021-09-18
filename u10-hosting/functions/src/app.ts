bdee3bca871860745bc659336ae0adf85896d7e5
yodsanorin <86304402+yodsanorin@users.noreply.github.com>
2021-09-18 18:06:02 +0700

    [U10] error

--- u10-hosting/functions/src/app.ts
+++ u10-hosting/functions/src/app.ts
@@ -3,3 +3,5 @@ export const app = express()
 app.get('/', (req, res) => res.send('Hello BC'))
 app.get('/cheese', (req, res) => res.send('Cheese & bread'))
 console.log('reloading...')
+
+const incompleteWork = 123
